0ef7e2651c22a699cdde04adceff3422746fefdd
Jerry Lundström <lundstrom.jerry@gmail.com>
2018-07-04 11:06:01 +0200

    Tests, v0.9.4
    - Add tests
    - Alpha release v0.9.4
    - `debian`:
      - Build using `gcc-5` if `gcc` is v4 (affects Trusty)
      - Add version to some dependencies
    - `rpm`: Add version to some dependencies

---
+++ src/test/test4.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+# Copyright (c) 2018, OARC, Inc.
+# All rights reserved.
+#
+# This file is part of dnsjit.
+#
+# dnsjit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dnsjit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dnsjit.  If not, see <http://www.gnu.org/licenses/>.
+
+../dnsjit "$srcdir/../../examples/readme.lua" dns.pcap-dist >test4.out
+diff "$srcdir/test4.gold" test4.out
