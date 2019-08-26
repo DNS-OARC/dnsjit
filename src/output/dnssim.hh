528003c5aee6c14e8c52812704de7133b25a13c2
Tomas Krizek <tomas.krizek@nic.cz>
2019-08-26 14:35:34 +0200

    output/dnssim: use private structure for uv_loop

--- src/output/dnssim.hh
+++ src/output/dnssim.hh
@@ -22,22 +22,22 @@
 //lua:require("dnsjit.core.receiver_h")
 
 typedef enum output_dnssim_transport {
-    OUTPUT_DNSSIM_TRANSPORT_UDP_ONLY,
-    OUTPUT_DNSSIM_TRANSPORT_UDP,
-    OUTPUT_DNSSIM_TRANSPORT_TCP,
-    OUTPUT_DNSSIM_TRANSPORT_TLS
+    OUTPUT_DNSSIM_TRANSPORT_UDP_ONLY = 255,
+    OUTPUT_DNSSIM_TRANSPORT_UDP= 254,
+    OUTPUT_DNSSIM_TRANSPORT_TCP = 253,
+    OUTPUT_DNSSIM_TRANSPORT_TLS = 252
 } output_dnssim_transport_t;
 
 typedef struct output_dnssim {
     core_log_t _log;
     output_dnssim_transport_t transport;
-    uv_loop_t loop;
 } output_dnssim_t;
 
 core_log_t* output_dnssim_log();
 
-void output_dnssim_init(output_dnssim_t* self);
-void output_dnssim_destroy(output_dnssim_t* self);
+output_dnssim_t* output_dnssim_new();
+void output_dnssim_free(output_dnssim_t* self);
+
 int output_dnssim_run_nowait(output_dnssim_t* self);
 
 core_receiver_t output_dnssim_receiver();
