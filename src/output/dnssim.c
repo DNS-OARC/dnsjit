d670a5553f696a36c78a35c66fdc1fc1afe9fba8
Tomas Krizek <tomas.krizek@nic.cz>
2019-11-29 12:04:06 +0100

    output/dnssim: add skeleton files from output.null

---
+++ src/output/dnssim.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2019, CZ.NIC, z.s.p.o.
+ * All rights reserved.
+ *
+ * This file is part of dnsjit.
+ *
+ * dnsjit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dnsjit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dnsjit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "output/dnssim.h"
+#include "core/assert.h"
+#include "core/object/pcap.h"
+
+static core_log_t    _log      = LOG_T_INIT("output.dnssim");
+static output_dnssim_t _defaults = {
+    LOG_T_INIT_OBJ("output.dnssim"),
+    0, 0, 0
+};
+
+core_log_t* output_dnssim_log()
+{
+    return &_log;
+}
+
+void output_dnssim_init(output_dnssim_t* self)
+{
+    mlassert_self();
+
+    *self = _defaults;
+}
+
+void output_dnssim_destroy(output_dnssim_t* self)
+{
+    mlassert_self();
+}
+
+static void _receive(output_dnssim_t* self, const core_object_t* obj)
+{
+    mlassert_self();
+
+    self->pkts++;
+}
+
+core_receiver_t output_dnssim_receiver()
+{
+    return (core_receiver_t)_receive;
+}
+
+void output_dnssim_run(output_dnssim_t* self, int64_t num)
+{
+    mlassert_self();
+
+    if (!self->prod) {
+        lfatal("no producer set");
+    }
+
+    if (num > 0) {
+        while (num--) {
+            const core_object_t* obj = self->prod(self->ctx);
+            if (!obj)
+                break;
+
+            self->pkts++;
+        }
+    } else {
+        for (;;) {
+            const core_object_t* obj = self->prod(self->ctx);
+            if (!obj)
+                break;
+
+            self->pkts++;
+        }
+    }
+}
