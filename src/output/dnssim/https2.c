DNS-OARC/dnsjit

commit 15a21daf86ebcf6da6599f7b0d66ebc32f76a9fe

Sonarcloud
- `dnssim`: Fix some of the security hotspots reported by SonarCloud

src/output/dnssim/https2.c
@@ -334,9 +334,9 @@ void _output_dnssim_https2_process_input_data(_output_dnssim_connection_t* conn,
     mlassert(conn->http2->session, "conn must have http2 session");
 
     /* Process incoming frames. */
-    ssize_t ret = 0;
+    ssize_t ret         = 0;
     conn->prevent_close = true;
-    ret         = nghttp2_session_mem_recv(conn->http2->session, (uint8_t*)data, len);
+    ret                 = nghttp2_session_mem_recv(conn->http2->session, (uint8_t*)data, len);
     conn->prevent_close = false;
     if (ret < 0) {
         mlwarning("failed nghttp2_session_mem_recv: %s", nghttp2_strerror(ret));
@@ -421,19 +421,22 @@ static int _http2_send_query_get(_output_dnssim_connection_t* conn, _output_dnss
     output_dnssim_t*       self    = conn->client->dnssim;
     core_object_payload_t* content = qry->qry.req->payload;
 
-    const size_t path_len = strlen(_self->h2_uri_path) + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) + (content->len * 4) / 3 + 3; /* upper limit of base64 encoding */
+    const size_t uri_path_len = strlen(_self->h2_uri_path);
+    const size_t path_len     = uri_path_len
+                            + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1
+                            + (content->len * 4) / 3 + 3; /* upper limit of base64 encoding */
     if (path_len >= _MAX_URI_LEN) {
         self->discarded++;
         linfo("http2: uri path with query too long, query discarded");
         return 0;
     }
     char path[path_len];
-    strncpy(path, _self->h2_uri_path, path_len);
-    strncat(path, OUTPUT_DNSSIM_HTTP_GET_TEMPLATE, path_len);
+    memcpy(path, _self->h2_uri_path, uri_path_len);
+    memcpy(&path[uri_path_len], OUTPUT_DNSSIM_HTTP_GET_TEMPLATE, sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1);
 
-    size_t  tmp_path_len = strlen(path);
-    int32_t ret          = base64url_encode(content->payload, content->len,
-        (uint8_t*)(path + tmp_path_len), path_len - tmp_path_len - 1);
+    int32_t ret = base64url_encode(content->payload, content->len,
+        (uint8_t*)&path[uri_path_len + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1],
+        sizeof(path) - uri_path_len - sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1);
     if (ret < 0) {
         self->discarded++;
         linfo("http2: base64url encode of query failed, query discarded");
@@ -444,7 +447,7 @@ static int _http2_send_query_get(_output_dnssim_connection_t* conn, _output_dnss
         OUTPUT_DNSSIM_MAKE_NV2(":method", "GET"),
         OUTPUT_DNSSIM_MAKE_NV2(":scheme", "https"),
         OUTPUT_DNSSIM_MAKE_NV(":authority", _self->h2_uri_authority, strlen(_self->h2_uri_authority)),
-        OUTPUT_DNSSIM_MAKE_NV(":path", path, tmp_path_len + ret),
+        OUTPUT_DNSSIM_MAKE_NV(":path", path, uri_path_len + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1 + ret),
         OUTPUT_DNSSIM_MAKE_NV2("accept", "application/dns-message"),
     };
 

src/output/dnssim.c
@@ -30,7 +30,7 @@
 #include <gnutls/gnutls.h>
 #include <string.h>
 
-static core_log_t _log = LOG_T_INIT("output.dnssim");
+static core_log_t      _log      = LOG_T_INIT("output.dnssim");
 static output_dnssim_t _defaults = { LOG_T_INIT_OBJ("output.dnssim") };
 
 static uint64_t _now_ms()
@@ -61,7 +61,7 @@ output_dnssim_t* output_dnssim_new(size_t max_clients)
     int              ret, i;
 
     mlfatal_oom(self = calloc(1, sizeof(_output_dnssim_t)));
-    *self = _defaults;
+    *self                      = _defaults;
     self->handshake_timeout_ms = 5000;
     self->idle_timeout_ms      = 10000;
     output_dnssim_timeout_ms(self, 2000);
@@ -145,7 +145,7 @@ void output_dnssim_log_name(output_dnssim_t* self, const char* name)
 
     strncpy(self->_log.name, name, sizeof(self->_log.name) - 1);
     self->_log.name[sizeof(self->_log.name) - 1] = 0;
-    self->_log.is_obj = false;
+    self->_log.is_obj                            = false;
 }
 
 static uint32_t _extract_client(const core_object_t* obj)
@@ -266,7 +266,7 @@ void output_dnssim_set_transport(output_dnssim_t* self, output_dnssim_transport_
     case OUTPUT_DNSSIM_TRANSPORT_HTTPS2:
 #if GNUTLS_VERSION_NUMBER >= DNSSIM_MIN_GNUTLS_VERSION
         lnotice("transport set to HTTP/2 over TLS");
-        if (strlen(_self->h2_uri_authority) > 0)
+        if (&_self->h2_uri_authority[0])
             lnotice("set uri authority to: %s", _self->h2_uri_authority);
 #else
         lfatal(DNSSIM_MIN_GNUTLS_ERRORMSG);

src/output/dnssim/common.c
@@ -38,13 +38,13 @@ static void _on_request_timeout(uv_timer_t* handle)
     _close_request((_output_dnssim_request_t*)handle->data);
 }
 
-static ssize_t parse_qsection(core_object_dns_t *dns)
+static ssize_t parse_qsection(core_object_dns_t* dns)
 {
-    core_object_dns_q_t q;
+    core_object_dns_q_t            q;
     static core_object_dns_label_t labels[MAX_LABELS];
-    const uint8_t *start;
-    int i;
-    int ret;
+    const uint8_t*                 start;
+    int                            i;
+    int                            ret;
 
     if (!dns || !dns->have_qdcount)
         return -1;
@@ -60,10 +60,10 @@ static ssize_t parse_qsection(core_object_dns_t *dns)
     return (dns->at - start);
 }
 
-int _output_dnssim_answers_request(_output_dnssim_request_t *req, core_object_dns_t *response)
+int _output_dnssim_answers_request(_output_dnssim_request_t* req, core_object_dns_t* response)
 {
-    const uint8_t *question;
-    ssize_t len;
+    const uint8_t* question;
+    ssize_t        len;
 
     if (!response->have_id || !response->have_qdcount)
         return _ERR_MALFORMED;
@@ -75,7 +75,7 @@ int _output_dnssim_answers_request(_output_dnssim_request_t *req, core_object_dn
         return _ERR_QUESTION;
 
     question = response->at;
-    len = parse_qsection(response);
+    len      = parse_qsection(response);
 
     if (req->question_len != len)
         return _ERR_QUESTION;
@@ -110,7 +110,7 @@ void _output_dnssim_create_request(output_dnssim_t* self, _output_dnssim_client_
         goto failure;
     }
 
-    req->question = req->dns_q->at;
+    req->question     = req->dns_q->at;
     req->question_len = parse_qsection(req->dns_q);
     if (req->question_len < 0) {
         ldebug("discarded malformed dns query: invalid question");

src/output/dnssim/internal.h
@@ -199,14 +199,14 @@ struct _output_dnssim_connection {
      * Numeric ordering of constants is significant and follows the typical connection lifecycle.
      * Ensure new states are added to a proper place. */
     enum {
-        _OUTPUT_DNSSIM_CONN_INITIALIZED   = 0,
-        _OUTPUT_DNSSIM_CONN_TCP_HANDSHAKE = 10,
-        _OUTPUT_DNSSIM_CONN_TLS_HANDSHAKE = 20,
-        _OUTPUT_DNSSIM_CONN_ACTIVE        = 30,
-        _OUTPUT_DNSSIM_CONN_CONGESTED     = 35,
+        _OUTPUT_DNSSIM_CONN_INITIALIZED     = 0,
+        _OUTPUT_DNSSIM_CONN_TCP_HANDSHAKE   = 10,
+        _OUTPUT_DNSSIM_CONN_TLS_HANDSHAKE   = 20,
+        _OUTPUT_DNSSIM_CONN_ACTIVE          = 30,
+        _OUTPUT_DNSSIM_CONN_CONGESTED       = 35,
         _OUTPUT_DNSSIM_CONN_CLOSE_REQUESTED = 38,
-        _OUTPUT_DNSSIM_CONN_CLOSING       = 40,
-        _OUTPUT_DNSSIM_CONN_CLOSED        = 50
+        _OUTPUT_DNSSIM_CONN_CLOSING         = 40,
+        _OUTPUT_DNSSIM_CONN_CLOSED          = 50
     } state;
 
     /* State of the data stream read. */
@@ -306,7 +306,7 @@ int  _output_dnssim_create_query_udp(output_dnssim_t* self, _output_dnssim_reque
 int  _output_dnssim_create_query_tcp(output_dnssim_t* self, _output_dnssim_request_t* req);
 void _output_dnssim_close_query_udp(_output_dnssim_query_udp_t* qry);
 void _output_dnssim_close_query_tcp(_output_dnssim_query_tcp_t* qry);
-int _output_dnssim_answers_request(_output_dnssim_request_t *req, core_object_dns_t *response);
+int  _output_dnssim_answers_request(_output_dnssim_request_t* req, core_object_dns_t* response);
 void _output_dnssim_request_answered(_output_dnssim_request_t* req, core_object_dns_t* msg);
 void _output_dnssim_maybe_free_request(_output_dnssim_request_t* req);
 void _output_dnssim_on_uv_alloc(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);

commit 63cf0a444d8f0bdd5e8fdb9726d16e30ef4b74cf

output/dnssim: fix regression in DoH GET
The path_len was missing space for terminating null byte.

The output length for base64url_encode() was incorrect, since the space
for terminating null byte was deducted twice - once indirectly with
sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) and second time explicitly with
-1.

The regression was introduced in 15a21daf86ebcf6da6599f7b0d66ebc32f76a9fe

src/output/dnssim/https2.c
@@ -44,6 +44,7 @@
     }
 
 #define OUTPUT_DNSSIM_HTTP_GET_TEMPLATE "?dns="
+#define OUTPUT_DNSSIM_HTTP_GET_TEMPLATE_LEN (sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1)
 #define OUTPUT_DNSSIM_HTTP2_INITIAL_MAX_CONCURRENT_STREAMS 100
 #define OUTPUT_DNSSIM_HTTP2_DEFAULT_MAX_CONCURRENT_STREAMS 0xffffffffu
 
@@ -423,8 +424,9 @@ static int _http2_send_query_get(_output_dnssim_connection_t* conn, _output_dnss
 
     const size_t uri_path_len = strlen(_self->h2_uri_path);
     const size_t path_len     = uri_path_len
-                            + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1
-                            + (content->len * 4) / 3 + 3; /* upper limit of base64 encoding */
+                            + OUTPUT_DNSSIM_HTTP_GET_TEMPLATE_LEN
+                            + (content->len * 4) / 3 + 3  /* upper limit of base64 encoding */
+                            + 1;  /* terminating null byte */
     if (path_len >= _MAX_URI_LEN) {
         self->discarded++;
         linfo("http2: uri path with query too long, query discarded");
@@ -432,11 +434,11 @@ static int _http2_send_query_get(_output_dnssim_connection_t* conn, _output_dnss
     }
     char path[path_len];
     memcpy(path, _self->h2_uri_path, uri_path_len);
-    memcpy(&path[uri_path_len], OUTPUT_DNSSIM_HTTP_GET_TEMPLATE, sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1);
+    memcpy(&path[uri_path_len], OUTPUT_DNSSIM_HTTP_GET_TEMPLATE, OUTPUT_DNSSIM_HTTP_GET_TEMPLATE_LEN);
 
     int32_t ret = base64url_encode(content->payload, content->len,
-        (uint8_t*)&path[uri_path_len + sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1],
-        sizeof(path) - uri_path_len - sizeof(OUTPUT_DNSSIM_HTTP_GET_TEMPLATE) - 1);
+        (uint8_t*)&path[uri_path_len + OUTPUT_DNSSIM_HTTP_GET_TEMPLATE_LEN],
+        sizeof(path) - uri_path_len - OUTPUT_DNSSIM_HTTP_GET_TEMPLATE_LEN - 1);
     if (ret < 0) {
         self->discarded++;
         linfo("http2: base64url encode of query failed, query discarded");